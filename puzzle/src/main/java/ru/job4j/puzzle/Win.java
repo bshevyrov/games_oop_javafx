74daf1bf1b9c4efd132461a5e47e0ace1debabf5
bohdan <bshevyrov@gmail.com>
2021-07-27 22:22:10 +0300

    2. Сокобан. Тестовое задание. [#75283 #182307

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,26 +1,36 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean check(int[][] board) {
+    public static boolean monoHorizontal(int[][] board, int i) {
         boolean rsl = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][0] == 1) {
-                for (int j = 0; j < board.length; j++) {
-                    if (board[i][j] == 0) {
-                        rsl = false;
-                        break;
-                    }
-                }
+        for (int j = 0; j < board.length; j++) {
+            if (board[i][j] == 0) {
+                rsl = false;
+                break;
             }
         }
+        return rsl;
+    }
+
+
+    public static boolean monoVertical(int[][] board, int j) {
+
+        boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[0][i] == 1) {
-                for (int j = 0; j < board.length; j++) {
-                    if (board[j][i] == 0) {
-                        rsl = false;
-                        break;
-                    }
-                }
+            if (board[i][j] == 0) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean check(int[][] board) {
+        boolean rsl = false;
+
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                rsl = true;
             }
         }
         return rsl;
